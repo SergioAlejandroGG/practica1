8622e65a250328d37bef5de153a09ebbb8a4aafb
Sergio Alejandro Galvan Gastelum <kingbluedude@gmail.com>
2024-02-27 12:18:40 -0700

    end of day commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,9 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'my-first-project';
+  description = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, ducimus aliquam, saepe ex quasi iste delectus aspernatur quis, mollitia minima laboriosam et ipsa quidem alias in. Ab distinctio voluptate laudantium."
+card = {
+  title: 'Titulo principal',
+  description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, ducimus aliquam, saepe ex quasi iste delectus aspernatur quis, mollitia minima laboriosam et ipsa quidem alias in. Ab distinctio voluptate laudantium." 
+}
 }
