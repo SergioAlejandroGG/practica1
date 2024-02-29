e20caaa3da87661abc26369f3d3806e97724d166
Sergio Alejandro Galvan Gastelum <kingbluedude@gmail.com>
2024-02-29 10:07:40 -0700

    created router outlet

--- src/app/segundo-dia/segundo-dia.component.ts
+++ src/app/segundo-dia/segundo-dia.component.ts
@@ -1,12 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-segundo-dia',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './segundo-dia.component.html',
   styleUrl: './segundo-dia.component.scss'
 })
 export class SegundoDiaComponent {
+variable=''
+// user = [
+//   (
+//     name: 'luis 1',
+//     description: 'descrp 1'
+//   )
+//   (
+//     name: 'luis 2',
+//     description: 'descrp 2'
+//   )
+//   (
+//     name: 'luis 3',
+//     description: 'descrp 3'
+//   )
+// ]
 
 }
