15cb2795a2343ec387eddfcc943c335c55f1bc74
Sergio Alejandro Galvan Gastelum <kingbluedude@gmail.com>
2024-03-01 13:09:26 -0700

    end of final profect.

--- src/app/proyecto-final/proyecto-final.component.ts
+++ src/app/proyecto-final/proyecto-final.component.ts
@@ -1,18 +1,32 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { GoogleMapsModule } from '@angular/google-maps';
-
+import { ExampleServiceService } from '../service/example-service.service';
 
 @Component({
   selector: 'app-proyecto-final',
   standalone: true,
-  imports: [CommonModule,GoogleMapsModule],
+  imports: [CommonModule,GoogleMapsModule,ReactiveFormsModule,FormsModule],
   templateUrl: './proyecto-final.component.html',
   styleUrl: './proyecto-final.component.scss'
 })
 
 
 export class ProyectoFinalComponent {
+
+  name = '';
+  users: any = []
+
+  constructor(public ExampleService: ExampleServiceService){
+  
+  }
+
+  fname (text: any){
+    console.log(text)
+    return text
+
+  }
 
   mapOptions: google.maps.MapOptions = {
     center: { lat: 25.788416, lng: -108.986368 },
@@ -21,6 +35,8 @@ export class ProyectoFinalComponent {
  marker = {
     position: { lat: 25.788416, lng: -108.986368 },
  }
+
+
 
 
 
